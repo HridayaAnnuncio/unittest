8975f27bb5e5e3bc2d158809e933bdfa3734ac0f
HridayaAnnuncio <Hridaya.Annuncio@st.niituniversity.in>
2019-11-15 18:47:59 +0530

    1st

---
+++ src/pk/addtest.java
@@ -0,0 +1,18 @@
+package pk;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class addtest {
+
+	@Test
+	public void test() {
+		jj a = new jj();
+		int result= a.add(3, 4);
+		assertEquals(7,result);
+		
+		
+	}
+
+}
---
+++ src/pk/jj.java
@@ -0,0 +1,12 @@
+package pk;
+
+public class jj {
+ public int add(int a, int b )
+ {
+	return a+b; 
+ }
+ public String concat (String a, String b)
+ {
+	 return a+b;
+ }
+}
